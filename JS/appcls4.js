54309e72634bddcc7977f81751db7ef186addf83
muqaddus <muqadduszehra990@gmail>
2025-08-06 22:46:17 -0700

    operator

---
+++ JS/appcls4.js
@@ -0,0 +1,24 @@
+// Arithmetics operator
+let num1 = prompt("Enter first Numkber");
+let num2 = prompt("Enter Second Number");
+
+// let result = num1 + num2;
+let result = num1 - num2;
+// let result = num1 * num2;
+// let result = num1 / num2;
+// let result = num1 % num2;
+
+
+
+document.getElementById("para").innerHTML = result;
+
+// // increment  decrement operator
+
+// let a = 8;
+// let b = 5;
+//  let result = a++ + b + ++a + a + --b + a++
+// // alert(result)
+// document.write(result)
+
+// let firstName;  //declare
+// firstName = "aptech"
