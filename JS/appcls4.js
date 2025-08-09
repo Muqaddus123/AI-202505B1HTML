240d6cbb0d277344a63bc1cb7518a213d0cd18f5
muqaddus <muqadduszehra990@gmail>
2025-08-08 23:00:54 -0700

    relational

--- JS/appcls4.js
+++ JS/appcls4.js
@@ -1,24 +1,51 @@
-// Arithmetics operator
-let num1 = prompt("Enter first Numkber");
-let num2 = prompt("Enter Second Number");
+// // Arithmetics operator
+// let num1 = +prompt("Enter first Num                                                                                                                                                                                                                                                                          ber");
+// let num2 = +prompt("Enter Second Number");
 
 // let result = num1 + num2;
-let result = num1 - num2;
-// let result = num1 * num2;
-// let result = num1 / num2;
-// let result = num1 % num2;
+// // let result = num1 - num2;
+// // let result = num1 * num2;
+// // let result = num1 / num2;
+// // let result = num1 % num2;
 
 
 
-document.getElementById("para").innerHTML = result;
+// document.getElementById("para").innerHTML = result;
 
-// // increment  decrement operator
+// // // increment  decrement operator
 
-// let a = 8;
-// let b = 5;
-//  let result = a++ + b + ++a + a + --b + a++
+// // let a = 8;
+// // let b = 5;
+// //  let result = a++ + b + ++a + a + --b + a++
+// // // alert(result)
+// // document.write(result)
+
+// // let firstName;  //declare
+// // firstName = "aptech"
+
+// let a = +prompt("Enter Your First")
+// let b = +prompt("Enter Your Second")
+
+// let result = a + b;
+// // document.write(result)
 // // alert(result)
+// // document.getElementById("para").innerHTML = result
 // document.write(result)
 
-// let firstName;  //declare
-// firstName = "aptech"
+
+
+//Relational operators
+// ==, ===, !=, !==, <, >, <=, >=
+
+let a = prompt("Enter first Value")
+let b = +prompt("Enter Second Value")
+
+// let result = a==b
+// let result = a!=b
+// let result = a===b
+// let result = a!==b
+// let result = a<b
+// let result = a>b
+let result 
+// let result = a<=b
+document.write(result)
