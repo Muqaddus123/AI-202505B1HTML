ea84545e49b00ddf928efba23291032bba258b43
muqaddus <muqadduszehra990@gmail>
2025-08-11 22:59:30 -0700

    mmmmmmmmmm

--- JS/appcls4.js
+++ JS/appcls4.js
@@ -10,42 +10,106 @@
 
 
 
-// document.getElementById("para").innerHTML = result;
+// // document.getElementById("para").innerHTML = result;
 
-// // // increment  decrement operator
+// // // // increment  decrement operator
 
-// // let a = 8;
-// // let b = 5;
-// //  let result = a++ + b + ++a + a + --b + a++
+// // // let a = 8;
+// // // let b = 5;
+// // //  let result = a++ + b + ++a + a + --b + a++
+// // // // alert(result)
+// // // document.write(result)
+
+// // // let firstName;  //declare
+// // // firstName = "aptech"
+
+// // let a = +prompt("Enter Your First")
+// // let b = +prompt("Enter Your Second")
+
+// // let result = a + b;
+// // // document.write(result)
 // // // alert(result)
+// // // document.getElementById("para").innerHTML = result
 // // document.write(result)
 
-// // let firstName;  //declare
-// // firstName = "aptech"
 
-// let a = +prompt("Enter Your First")
-// let b = +prompt("Enter Your Second")
 
-// let result = a + b;
-// // document.write(result)
-// // alert(result)
-// // document.getElementById("para").innerHTML = result
+// //Relational operators
+// // ==, ===, !=, !==, <, >, <=, >=
+
+// let a = prompt("Enter first Value")
+// let b = +prompt("Enter Second Value")
+
+// let result = a==b
+// // let result = a!=b
+// // let result = a===b
+// // let result = a!==b
+// // let result = a<b
+// // let result = a>b
+// let result 
+// // let result = a<=b
 // document.write(result)
 
 
+// Logical operaters
 
-//Relational operators
-// ==, ===, !=, !==, <, >, <=, >=
+// // AND && OR  || NOT !
 
-let a = prompt("Enter first Value")
-let b = +prompt("Enter Second Value")
+// // let num1 = 20;
+// // let num2 = 30;
+// //  let result = num1 <= num2 && num1 ==num2
+// //  document.getElementById("para").innerHTML =result
 
-// let result = a==b
-// let result = a!=b
-// let result = a===b
-// let result = a!==b
-// let result = a<b
-// let result = a>b
-let result 
-// let result = a<=b
-document.write(result)
+// // let num1 = 20;
+// // let num2 = 30;
+// // let num3 = 10;
+
+// //  let result = num1 <= num2 || num1 == num2 || num3 > num1
+// //  document.getElementById("para").innerHTML =result
+
+// // let num1 = 20;
+// // let num2 = 30;
+// // let num3 = 10;
+
+// //  let result = !(num1 <= num2 && num1 != num2 && num3 > num1)
+// //  document.getElementById("para").innerHTML =result
+
+// // Assigment Operators
+// let a = 20;
+// let b = 10;
+
+// // // document.write(a);
+// // a += b
+// // a -= b
+// a *= b
+// // a /= b
+// // a %= b
+// document.write(a);
+
+// // ternary operators
+
+// // let num1 = 40;
+// // let num2 = 20;
+// // // let result = num1 > num2 ? "this is true" : "False"
+// // let result = num1 < num2 ? "this us true" : "false"
+// // document.getElementById("para").innerHTML = result
+
+// #if and else
+
+    // let name = "aptech sfc"
+    // if(name == "aptech sfc"){
+    //     document.write("welcome to aptech sfc")
+    // }
+    // else{
+    //     document.write("go to aptech msg")
+    // }
+
+       let num =  prompt("enter your number")
+    if(num > 0)
+    {
+        document.write("this is +ve number")
+    }
+    else
+    {
+        document.write("this is -ve number")
+    }
